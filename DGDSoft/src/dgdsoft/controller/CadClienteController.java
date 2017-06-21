e365be6c8d8c4a14080920becbad984bb0dabf18
diego <diego@diego-PC>
2017-06-21 16:52:28 -0300

    compete cad client, show all informations normaly

--- DGDSoft/src/dgdsoft/controller/CadClienteController.java
+++ DGDSoft/src/dgdsoft/controller/CadClienteController.java
@@ -1,13 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dgdsoft.controller;
 
+import dgdsoft.model.dao.CidEstDao;
+import dgdsoft.model.dao.ClienteDAO;
+import dgdsoft.model.database.Database;
+import dgdsoft.model.database.DatabaseFactory;
+import dgdsoft.model.domain.Cliente;
 import java.net.URL;
+import java.sql.Connection;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +26,89 @@ import javafx.fxml.Initializable;
  */
 public class CadClienteController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML    private TableView<Cliente> tableViewClientes;
+    @FXML    private TableColumn<Cliente, String> tableColumnClienteNome;
+    @FXML    private Button btnInserir;
+    @FXML    private Button btnAlterar;
+    @FXML    private Button btnExcluir;
+    @FXML    private Label labelClienteCodigo;
+    @FXML    private Label ClienteNome;
+    @FXML    private Label boxSexo;
+    @FXML    private Label boxNacionalidade;
+    @FXML    private Label boxEstCivil;
+    @FXML    private Label ClienteCPF;
+    @FXML    private Label ClienteRG;
+    @FXML    private Label ClienteNasc;
+    @FXML    private Label ClienteEnd;
+    @FXML    private Label ClienteNumero;
+    @FXML    private Label ClienteComp;
+    @FXML    private Label ClienteBairro;
+    @FXML    private Label ClienteUF;
+    @FXML    private Label ClienteCidade;
+    @FXML    private Label ClienteTelefone;
+    @FXML    private Label ClienteCelular;
+    @FXML    private Label ClienteEmail;
+    
+    @FXML
+    private List<Cliente> listClientes;
+    private ObservableList<Cliente> observableListClientes;
+       
+    //atributos para manipulação de banco de dados
+    private final Database database = DatabaseFactory.getDatabase("postgresql");
+    private final Connection connection = database.conectar();
+    private final ClienteDAO clienteDao = new ClienteDAO();
+    private final CidEstDao cidestDao = new CidEstDao();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        clienteDao.setConnection(connection); //conexao setada no cliente dao
+        cidestDao.setConnection(connection);
+        carregarTableVielCliente();
+        
+        
+        //listem acionado diante de qualquer alterações na seleção de itens da Tableview
+        tableViewClientes.getSelectionModel().selectedItemProperty().addListener(
+        (observable,oldValue,newValue) -> selecionarItemTableViewClientes(newValue));
     }    
+ 
+     public void carregarTableVielCliente(){
+        //configura as colunas para exibir o nome e cpf na tabela (TableView)
+        tableColumnClienteNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        //chama o metodo listar
+        listClientes = clienteDao.listar();
+        //converte um observableList para um arraylist
+        observableListClientes = FXCollections.observableArrayList(listClientes);
+        //passa o observablelist para a tableview
+        tableViewClientes.setItems(observableListClientes);
+    }
+    
+    public void selecionarItemTableViewClientes(Cliente cliente){
+        if(cliente != null){
+            labelClienteCodigo.setText(String.valueOf(cliente.getCdCliente()));
+            ClienteNome.setText(cliente.getNome());
+            ClienteCPF.setText(cliente.getCpf());
+            ClienteRG.setText(cliente.getRg());
+            ClienteEnd.setText(cliente.getEnd());
+            ClienteUF.setText(cliente.getUf());
+            ClienteBairro.setText(cliente.getBairro());
+            ClienteCidade.setText(cliente.getCidade());
+            ClienteTelefone.setText(cliente.getTelefone());
+            //IMAGEM
+            boxSexo.setText(cliente.getSexo());
+            boxNacionalidade.setText(cliente.getNacionalidade());
+            //CEP Ver se vai utilizar
+            //ClienteNasc.setText(String.valueOf(cliente.getNasc()));
+            boxEstCivil.setText(cliente.getEstCivil());
+            //ClienteCelular.setText(cliente.getCelular());
+            //ClienteEmail.setText(cliente.getEmail());
+            ClienteNumero.setText(String.valueOf(cliente.getNumeroEnd()));
+            //ClienteComp.setText(cliente.getComplemento());
+        }else{
+            labelClienteCodigo.setText("");
+            ClienteNome.setText("");
+            ClienteCPF.setText("");
+            ClienteTelefone.setText("");
+        }
+    }
     
 }
