61989f6e38bcae0e6d7d431e2c9ab74429cc27b6
diego <diego@diego-PC>
2017-06-20 21:19:09 -0300

    add windows Client, Fornec and Products

--- DGDSoft/src/dgdsoft/controller/CadastroController.java
+++ DGDSoft/src/dgdsoft/controller/CadastroController.java
@@ -1,9 +1,12 @@
 package dgdsoft.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -13,11 +16,32 @@ import javafx.scene.layout.AnchorPane;
  */
 public class CadastroController implements Initializable {
 
-    @FXML    private AnchorPane anchorPane;
-        
+    @FXML   private AnchorPane anchorPane;
+    @FXML   private Button btnCadCliente;  
+    @FXML   private Button btnCadFornecedor;
+    @FXML   private Button btnCadProduto;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    public void handleBtnCadastrosCliente() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/dgdsoft/view/CadCliente.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+    
+    @FXML
+    public void handleBtndastrosFornecedor() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/dgdsoft/view/CadFornecedor.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+    
+    @FXML
+    public void handleBtnCadastrosProduto() throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/dgdsoft/view/CadProduto.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
     
 }
