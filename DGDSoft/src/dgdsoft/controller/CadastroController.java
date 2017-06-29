e8a22d2903b1d777ce0bd6db631a526faacace27
diego <diego@diego-PC>
2017-06-29 20:51:15 -0300

    style

--- DGDSoft/src/dgdsoft/controller/CadastroController.java
+++ DGDSoft/src/dgdsoft/controller/CadastroController.java
@@ -10,12 +10,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -30,12 +28,32 @@ public class CadastroController implements Initializable {
     @FXML   private Button btnCadCliente;  
     @FXML   private Button btnCadFornecedor;
     @FXML   private Button btnCadProduto;
+    @FXML   private Button btnHome;
+   
     @FXML   private ImageView imgExit;
+    @FXML   private ImageView ingLoggout;
     @FXML   private ImageView imgInfo;
     
+    @FXML   private ImageView ingHome;
     @FXML   private ImageView ingCliente;
-       
+    @FXML   private ImageView ingFornecedor;
+    @FXML   private ImageView ingProduto;
    
+    String defultStyle = "-fx-border-width: 0px 0px 0px 5px;"
+            + "-fx-border-color:none";
+
+    String activeStyle = "-fx-border-width: 0px 0px 0px 5px;"
+            + "-fx-border-color:#FF4E3C";
+
+    //Image home = new Image("@../dgdsoft/imagem/home_1.png");
+    //Image homeRed = new Image("@../dgdsoft/imagem/homeRed.png");
+    //Image cliente = new Image("/dgdsoft/imagem/employe.png");
+    //Image clienteRed = new Image("/dgdsoft/imagem/employeRed.png");
+    //Image fornecedor = new Image("/dgdsoft/imagem/fornec.png");
+    //Image fornecedorRed = new Image("/dgdsoft/imagem/fornecRed.png");
+    //Image produto = new Image("/dgdsoft/imagem/stock.png");
+    //Image produtoRed = new Image("/dgdsoft/imagem/stockRed.png");
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -57,7 +75,28 @@ public class CadastroController implements Initializable {
         newStage.setScene(scene);
         newStage.show();
         imgInfo.getScene().getWindow().hide();
-
+    }
+    
+     @FXML
+    private void goHomeBB() throws IOException {
+        Stage newStage = new Stage();
+        newStage.initStyle(StageStyle.TRANSPARENT);
+        Parent parent = FXMLLoader.load(getClass().getResource("/dgdsoft/view/MainDGDPrincipal.fxml"));
+        Scene scene = new Scene(parent);
+        newStage.setScene(scene);
+        newStage.show();
+        btnHome.getScene().getWindow().hide();
+    }
+    
+     @FXML
+    private void goLogin(MouseEvent event) throws IOException {
+        Stage newStage = new Stage();
+        newStage.initStyle(StageStyle.TRANSPARENT);
+        Parent parent = FXMLLoader.load(getClass().getResource("/dgdsoft/view/MainDGD.fxml"));
+        Scene scene = new Scene(parent);
+        newStage.setScene(scene);
+        newStage.show();
+        ingLoggout.getScene().getWindow().hide();
     }
     
     @FXML
@@ -79,5 +118,15 @@ public class CadastroController implements Initializable {
         anchorPane.getChildren().setAll(a);
     }
     
-     
+    private void homeActive(){
+       //ingHome.setImage(homeRed);
+       //ingCliente.setImage(cliente);
+       //ingFornecedor.setImage(fornecedor);
+       //ingProduto.setImage(produto);
+       //btnHome.setStyle(activeStyle);
+       //btnCadCliente.setStyle(defultStyle);
+       //btnCadFornecedor.setStyle(defultStyle);
+       //btnCadProduto.setStyle(defultStyle);
+    }
+    
 }
